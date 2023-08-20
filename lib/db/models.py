88969953852cdee098ca2d0afb6bf7da9fd0f738
Aadil Ahmed <aadilahmed0@gmail.com>
2023-08-19 22:24:18 -0700

    create users table in models.py

---
+++ lib/db/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    username = Column(String)
+    email = Column(String(55))
+
+    def __repr__(self):
+        return f"\n<User" + \
+            f"id={self.id}, " + \
+            f"username={self.username}, " + \
+            f"email={self.email}, " \
+            + ">"
+        
+
